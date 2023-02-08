7a6dee651231b4a1952739120c75b15030b0829e
Jaime <jaimeg5297@gmail.com>
2023-02-08 12:17:43 -0500

    Utilizando ejemplos encontrados en la web con ChatGPT

--- server.py
+++ server.py
@@ -1,67 +1,29 @@
-from PyQt5 import QtCore, QtWebSockets, QtNetwork, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QAction
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5 import QtWebSockets
 
-class MyServer(QtCore.QObject):
-    def __init__(self, parent):
-        super(QtCore.QObject, self).__init__(parent)
+class WebSocketHandler(QObject):
+    message_received = pyqtSignal(str)
+
+    def __init__(self):
+        super(QObject,self).__init__()
+        self.server = QtWebSockets.QWebSocketServer("WebSocket Server", QtWebSockets.QWebSocketServer.NonSecureMode)
+        self.server.newConnection.connect(self.on_new_connection)
         self.clients = []
-        print("server name: {}".format(parent.serverName()))
-        self.server = QtWebSockets.QWebSocketServer(parent.serverName(), parent.secureMode(), parent)
-        if self.server.listen(QtNetwork.QHostAddress.LocalHost, 1302):
-            print('Listening: {}:{}:{}'.format(
-                self.server.serverName(), self.server.serverAddress().toString(),
-                str(self.server.serverPort())))
-        else:
-            print('error')
-        self.server.acceptError.connect(self.onAcceptError)
-        self.server.newConnection.connect(self.onNewConnection)
-        self.clientConnection = None
-        print(self.server.isListening())
 
-    def onAcceptError(accept_error):
-        print("Accept Error: {}".format(accept_error))
+    def on_new_connection(self):
+        socket = self.server.nextPendingConnection()
+        socket.textMessageReceived.connect(self.on_text_message_received)
+        socket.disconnected.connect(self.on_disconnected)
+        self.clients.append(socket)
 
-    def onNewConnection(self):
-        print("onNewConnection")
-        self.clientConnection = self.server.nextPendingConnection()
-        self.clientConnection.textMessageReceived.connect(self.processTextMessage)
+    def on_text_message_received(self, message):
+        self.message_received.emit(message)
 
-        self.clientConnection.textFrameReceived.connect(self.processTextFrame)
+    def on_disconnected(self):
+        socket = self.sender()
+        self.clients.remove(socket)
+        socket.deleteLater()
 
-        self.clientConnection.binaryMessageReceived.connect(self.processBinaryMessage)
-        self.clientConnection.disconnected.connect(self.socketDisconnected)
-
-        print("newClient")
-        self.clients.append(self.clientConnection)
-
-    def processTextFrame(self, frame, is_last_frame):
-        print("in processTextFrame")
-        print("\tFrame: {} ; is_last_frame: {}".format(frame, is_last_frame))
-
-    def processTextMessage(self, message):
-        print("processTextMessage - message: {}".format(message))
-        if self.clientConnection:
-            for client in self.clients:
-                # if client!= self.clientConnection:
-                client.sendTextMessage(message)
-            # self.clientConnection.sendTextMessage(message)
-
-    def processBinaryMessage(self, message):
-        print("b:",message)
-        if self.clientConnection:
-            self.clientConnection.sendBinaryMessage(message)
-
-    def socketDisconnected(self):
-        print("socketDisconnected")
-        if self.clientConnection:
-            self.clients.remove(self.clientConnection)
-            self.clientConnection.deleteLater()
-
-if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
-    serverObject = QtWebSockets.QWebSocketServer('My Socket', QtWebSockets.QWebSocketServer.NonSecureMode)
-    server = MyServer(serverObject)
-    serverObject.closed.connect(app.quit)
-    app.exec_()
+    def broadcast(self, message):
+        for client in self.clients:
+            client.sendTextMessage(message)
