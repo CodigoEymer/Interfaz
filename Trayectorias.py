09eea14a584ad2769786b4bc2eb9905b4b19ee73
eym <eymer.guaquez@correounivalle.edu.com>
2023-03-11 18:48:21 -0500

    Conseguimos obtener los wp de la primer lado de un cuadrado

--- Trayectorias.py
+++ Trayectorias.py
@@ -2,8 +2,8 @@ import math
 
 vertices = [(6,4), (12,4), (12,10), (6,10)]
 # Campo de vision
-LX = 1
-LY = 1
+LX = 1.0
+LY = 1.0
 # Sobrelapamiento minimo en X y Y
 Ovx = 0.1
 Ovy = 0.1
@@ -11,6 +11,8 @@ Ovy = 0.1
 dr = LX-Ovx
 # Distancia entre weypoints
 dw = LY-Ovy
+# Distancia entre vertices
+di = 0
 
 p = len(vertices)
 sum_x = sum([v[0] for v in vertices])
@@ -33,24 +35,13 @@ dcp = float("inf")
 
 wp_dron =[]
 
-for i in range(p): 
+for m in range(p): 
 
-    x1, y1 = vertices[i]
-    x2, y2 = vertices[(i + 1) % p]
+    x1, y1 = vertices[m]
+    x2, y2 = vertices[(m + 1) % p]
     dx = x2 - x1
     dy = y2 - y1
-
-    
-
     distance = abs(dx * (centroid_y - y1) - dy * (centroid_x - x1)) / math.sqrt(dx**2 + dy**2)
-
-    if dx == 0:
-        catetox = 0
-        catetoy = dw
-    else:
-        angulo = math.atan(dy/dx)    
-        catetox = math.cos(angulo)*dw
-        catetoy = math.sin(angulo)*dw
 
     if distance < dcp:
         dcp = distance
@@ -65,8 +56,7 @@ nr = 1
 
 for j in range(nr):
     for i in range(p):
-
-
+        
         x1, y1 = vertices[i]
         if i == p-1:
             x2, y2 = vertices[0]
@@ -84,28 +74,52 @@ for j in range(nr):
         vector2x = x3 - x2
         vector2y = y3 - y2
         angleY = angle_between_vectors(vector1x, vector1y, vector2x, vector2y)
+
+        angleY= angleY*math.pi/180
         print("angleY:", angleY)
 
         if i == 0 and j == 0:    
-            di = distancia(x1, y1, x2, y2)-(LX/(math.tan(angleY)))         
+            if angleY%90 != 0:
+                di = distancia(x1, y1, x2, y2)-(LX/(math.tan(angleY)))      
+            else:
+                di = distancia(x1, y1, x2, y2)
         else:
-            di = distancia(x1, y1, x2, y2)           
+                di = distancia(x1, y1, x2, y2)
 
-        print("di:", di)
+        print("di:", di)             
 
         # Numero de waypoints
         nw = int(round((di-Ovy)/dw))
 
-        for k in range(nw):
+        dx = x2 - x1
+        dy = y2 - y1
 
+        if dx == 0:
+            catetox = 0
+            catetoy = dw
+        else:
+            angulo = math.atan(dy/dx) 
+            print("angulo"+str(angulo))   
+            catetox = math.cos(angulo)*dw
+            catetoy = math.sin(angulo)*dw
+
+        for k in range(nw):
             if k == 0:
                 if i == 0 and j == 0:   
-                    wp_dron.append((x1,y1+LX/2))
+                    if dx == 0:
+                        catetoxO = 0
+                        catetoyO = dw
+                    else:  
+                        catetoxO = math.cos(angulo)*(LX/math.tan(angleY))
+                        catetoyO = math.sin(angulo)*(LX/math.tan(angleY))
+                    print((LX/2),(LY/2))
+                    wp_dron.append(((x1-catetoxO)+(LX/2),(y1-catetoyO)+(LY/2)))
+                    print("PointOrigen"+str( wp_dron[k]))
+
                 else:    
                     wp_dron.append((x1+(LY/2),y1+LX/2))
             else:
                 xwp , ywp = wp_dron[k-1]
-
                 wp_dron.append((xwp+catetox,ywp+catetoy))
                 di = distancia(x1, y1, x2, y2)-(LX/(math.tan(angleY)))
             
