ee6f73a05369386386826e7b0f5e3b88b58c6c6b
Jaime <jaimeg5297@gmail.com>
2023-03-18 17:40:01 -0500

    primer anillo logrado en el algoritmo

--- Trayectorias.py
+++ Trayectorias.py
@@ -1,16 +1,15 @@
 import math
-
-vertices = [(4,4), (8,2), (16,6), (12,12),(2,10)]
+#input
+vertices = [(-1,-2),(1,-2),(3,0),(1,1),(-1,1),(-3,0)]
+#output
+wp_dron =[]
 # Campo de vision
 LX = 1.0
 LY = 1.0
-# Sobrelapamiento minimo en X y Y
+# Sobrelapamiento minimo en Y
 Ovx = 0.1
-Ovy = 0.1
-# Distancia entre anillos
-dr = LX-Ovx
-# Distancia entre weypoints
-dw = LY-Ovy
+
+
 # Distancia entre vertices
 di = 0
 
@@ -19,6 +18,8 @@ sum_x = sum([v[0] for v in vertices])
 sum_y = sum([v[1] for v in vertices])
 centroid_x = sum_x / p
 centroid_y = sum_y / p
+
+cp=(centroid_x,centroid_y)
 
 def distancia(x1, y1, x2, y2):
     r= math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
@@ -32,32 +33,33 @@ def angle_between_vectors(x1, y1, x2, y2):
         tetha= -(math.acos(dot_product/mag_product))
     else:
         tetha= (math.acos(dot_product/mag_product))
-
     return (tetha)
-
 
 dcp = float("inf")
 
-wp_dron =[]
-
 for m in range(p): 
-
     x1, y1 = vertices[m]
     x2, y2 = vertices[(m + 1) % p]
     dx = x2 - x1
     dy = y2 - y1
     distance = abs(dx * (centroid_y - y1) - dy * (centroid_x - x1)) / math.sqrt(dx**2 + dy**2)
-
     if distance < dcp:
         dcp = distance
 
+# Distancia entre anillos
+dr = LX-Ovx
 
 # Numero de anillos
 nr = (dcp-Ovx)/dr
+num_rings = int(math.ceil(nr))
+num_rings=1
 
-nr = 1
+#Ovx recalculado
+#Ovx=(num_rings*LX-dcp)/(num_rings-1)
+#dr recalculado
+#dr=(dcp-LX)/(num_rings-1)
 
-for j in range(nr):
+for j in range(num_rings):
     for i in range(p):
         
         x0, y0 = vertices[i-1]
@@ -78,17 +80,33 @@ for j in range(nr):
         vector2x = float(x2 - x1)
         vector2y = float(y2 - y1)
         angleY = angle_between_vectors(vector1x, vector1y, vector2x, vector2y)
-        #print("angle interior: ", str(angleY*180/(math.pi)))
+        print("angle interior: ", str(angleY*180/(math.pi)))
+        
         if i == 0 and j == 0:    
-            if angleY%90 != 0:
-                di = distancia(x1, y1, x2, y2)-(LX/(math.tan(angleY)))      
-            else:
-                di = distancia(x1, y1, x2, y2)
+            di = distancia(x1, y1, x2, y2)-(LX/(math.tan(angleY)))      
         else:
-                di = distancia(x1, y1, x2, y2)
+            di = distancia(x1, y1, x2, y2)
+
+        # Sobrelapamiento minimo en Y
+        Ovy = 0.1
+
+        # Distancia entre weypoints
+        dw = LY-Ovy
 
         # Numero de waypoints
-        nw = int(round((di-Ovy)/dw))
+        nw = (di-Ovy)/dw
+        num_wp_line = int(math.ceil(nw))
+
+        if nw==1:
+            Ovy = 0.0
+        else:
+            # Ovy recalculado
+            #print(nw,LY,di,nw)    
+            Ovy = (num_wp_line*LY-di)/(num_wp_line-1)
+            #print("Ovy: ", str(Ovy))
+            # dw recalculado
+            dw = (di-LY)/(num_wp_line-1)
+            #print("dw: ", str(dw))
 
         dx = float(x2 - x1)
         dy = float(y2 - y1)
@@ -97,7 +115,7 @@ for j in range(nr):
         catetox = math.cos(angulo)*dw
         catetoy = math.sin(angulo)*dw
 
-        for k in range(nw):
+        for k in range(num_wp_line):
             if k == 0:
                 h=   math.sqrt((LX/2)**2 + (LY/2)**2)
                 angulo_phi=math.atan(LX/LY)
@@ -119,12 +137,11 @@ for j in range(nr):
                 indice=  (len(wp_dron))-1
                 xwp , ywp = wp_dron[indice]
                 wp_dron.append((xwp+catetox,ywp+catetoy))
-        
-        # Ovy recalculado
-        Ovy = (nw*LY-di)/(nw-1)
-        # dw recalculado
-        dw = (di-LY)/(nw-1)
 
-for l in range((len(wp_dron))):
-    #pass    
-    print(str( wp_dron[l]))
+            print(str(wp_dron[(len(wp_dron))-1]))
+        
+
+        
+        
+
+ 
